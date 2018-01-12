aa934a1decd567b3b037001c3567560f34c5e40c
Alexey Halaidzhy <a.halaidzhy@corp.mail.ru>
2018-01-12 07:39:14 +0300

    Занятие 7. Добавлен пример по виджетам

---
+++ lesson7/Widgets/app/src/main/java/com/my/examples/countdownwidget/MainActivity.java
@@ -0,0 +1,17 @@
+package com.my.examples.countdownwidget;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by a.halaidzhy on 12.01.2018.
+ */
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_main);
+    }
+}
